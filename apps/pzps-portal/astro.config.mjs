c6b475ff3af6bd34875c4cba6788a3806d842a71
Fronikuniu <Fronikuniu@gmail.com>
2024-03-08 16:37:28 +0100

    Add analogjs with angular to astro

--- apps/pzps-portal/astro.config.mjs
+++ apps/pzps-portal/astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import { fileURLToPath } from 'node:url';
+import angular from '@analogjs/astro-angular';
 
 // https://astro.build/config
 export default defineConfig({
   outDir: '../../dist/apps/pzps-portal',
   integrations: [
+    angular(),
     tailwind({
       configFile: fileURLToPath(
         new URL('./tailwind.config.cjs', import.meta.url)

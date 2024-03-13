3e2c2f98f797a6816adcf892698cf7874ee14b9b
Fronikuniu <Fronikuniu@gmail.com>
2024-03-13 21:32:08 +0100

    Add strapi and astro with angular

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-import analogjsangular from "@analogjs/astro-angular";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [analogjsangular()]
-});
